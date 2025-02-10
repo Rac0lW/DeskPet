1d66325bc7590c84e3ac301824c420da227d60b0
Revo Wu <rev027w@gmail.com>
2025-02-10 22:44:49 +0800

    Feature: Draging is active.

---
+++ drag.gd
@@ -0,0 +1,34 @@
+extends Label
+
+var is_entering:bool = false
+var is_draging:bool = false
+
+var diff:Vector2i
+
+func _input(event: InputEvent) -> void:
+	if is_entering == false:
+		return
+	
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				is_draging = true
+				diff = DisplayServer.mouse_get_position() - DisplayServer.window_get_position()
+			else:
+				is_draging = false
+		
+
+
+func _process(delta: float) -> void:
+	if is_draging == false:
+		return
+	
+	DisplayServer.window_set_position(DisplayServer.mouse_get_position() - diff)
+
+
+func _on_mouse_entered() -> void:
+	is_entering = true
+
+
+func _on_mouse_exited() -> void:
+	is_entering = false

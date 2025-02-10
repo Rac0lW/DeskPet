a2d16dc1daabb4e1fc7939d6abef6a4b3911a4ea
Revo Wu <rev027w@gmail.com>
2025-02-10 22:08:00 +0800

    CHange the shadow offset.

--- main.gd
+++ main.gd
@@ -4,17 +4,16 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	boot()
+
+
+func boot():
 	var win_pos = DisplayServer.window_get_position()
 	var screen_size = DisplayServer.screen_get_size()
 	
 	var right_pos_x = screen_size.x - offset - size.x
-	var right_pos_y = screen_size.y - offset - size.y
+	var right_pos_y = screen_size.y - offset - size.y - 48
 	
 	var right_pos:Vector2i = Vector2i(right_pos_x, right_pos_y)
 	
 	DisplayServer.window_set_position(right_pos)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass

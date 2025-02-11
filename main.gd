f448795070e413721019b90babf1df2f0dceb360
Revo Wu <rev027w@gmail.com>
2025-02-11 21:49:42 +0800

    Trying to add the OSX like tips, but failed.

--- main.gd
+++ main.gd
@@ -4,16 +4,63 @@ extends Control
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var text_edit: TextEdit = $MarginContainer/PanelContainer/VBoxContainer/TextEdit
 
+
+
+@export var hide_start_windows_pos:Vector2i
+@export var hide_end_windows_pos:Vector2i
+
+#var is_hiding:bool = false
+#var tween:Tween
 var save_manager:SaveGame = SaveGame.new()
+#@export var window_pos:Vector2i
+#-------------------------------------------------------------------------------
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Save"):
 		_save_game()
+		
+	#if event.is_action_pressed("Hide"):
+		#if is_hiding:
+			#is_hiding = false
+			#_cancel_hiding()
+		#else:
+			#is_hiding = true
+			#_start_hiding()
 
 func _ready() -> void:
 	boot()
 	
 	_load_game()
+	
+	#tween = create_tween()
+	#window_pos = DisplayServer.window_get_position()
+	#hide_start_windows_pos = DisplayServer.window_get_position()
+	#hide_end_windows_pos = hide_start_windows_pos + DisplayServer.window_get_size() + Vector2i(46, 46)
+	
+func _process(delta: float) -> void:
+	pass
+	#if is_hiding:
+		#DisplayServer.window_set_position(window_pos)
+	
+##-------------------------------------------------------------------------------
+#func _start_hiding() -> void:
+	#if is_hiding:
+		#return
+#
+	#window_pos = DisplayServer.window_get_position()
+	#tween.set_ease(Tween.EASE_OUT_IN)
+	#tween.tween_property(self, "window_pos", hide_end_windows_pos, 0.5)
+#
+#func _cancel_hiding() -> void:
+	#if !is_hiding:
+		#return
+		#
+	#window_pos = DisplayServer.window_get_position()
+	#tween.set_ease(Tween.EASE_OUT_IN)
+	#tween.tween_property(self, "window_pos", hide_start_windows_pos, 0.5)
+
+#-------------------------------------------------------------------------------
 
 func boot():
 	var win_pos = DisplayServer.window_get_position()
@@ -36,6 +83,18 @@ func _save_game() -> void:
 	save_manager.text_save = text_edit.text
 	save_manager.save_game()
 
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+
+
+
+
+
+#-------------------------------------------------------------------------------
 
 func _on_text_edit_text_changed() -> void:
 	_save_game()

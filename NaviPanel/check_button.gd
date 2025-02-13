f0c7fa62aa281d32ade7b13723b177dc924394d5
Revo Wu <rev027w@gmail.com>
2025-02-13 19:41:40 +0800

    Fix: Fixed the bug that makes the toggle button work unexpectily.

--- NaviPanel/check_button.gd
+++ NaviPanel/check_button.gd
@@ -1,11 +1,5 @@
 extends CheckButton
 
 
-
-func _on_button_down() -> void:
-	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_ALWAYS_ON_TOP, true)
-
-
-func _on_button_up() -> void:
-	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_ALWAYS_ON_TOP, false)
-	
+func _on_toggled(toggled_on: bool) -> void:
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_ALWAYS_ON_TOP, toggled_on)
--- text_edit.gd
+++ text_edit.gd
@@ -8,7 +8,9 @@ var target_label:Label
 func _on_text_changed() -> void:
 	target_label = get_tree().get_nodes_in_group("Label").back()
 	var lines = text.split("\n")
+	var index:int = 0
+	#TODO:make it a non-"" value on it
 	if lines.size() > 0:
-		target_label.text = lines[0]
+		target_label.text = lines[index]
 	else:
 		target_label.text = ""

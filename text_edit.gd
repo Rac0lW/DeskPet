3db47416c1511934140b9aa79aaec0cd27d42ee3
Revo Wu <rev027w@gmail.com>
2025-02-13 16:58:59 +0800

    Fix: fix bugs that title label can't read.

--- text_edit.gd
+++ text_edit.gd
@@ -1,12 +1,12 @@
 extends TextEdit
 
-
-@export var target_label:Label
+var target_label:Label
 
 
 
 
 func _on_text_changed() -> void:
+	target_label = get_tree().get_nodes_in_group("Label").back()
 	var lines = text.split("\n")
 	if lines.size() > 0:
 		target_label.text = lines[0]

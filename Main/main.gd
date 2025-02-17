ec1e849e77be8712cf578dcdb27da882b6074c47
Revo Wu <rev027w@gmail.com>
2025-02-17 11:19:44 +0800

    Add: add the OS-like sidenote style.

--- Main/main.gd
+++ Main/main.gd
@@ -1,29 +1,41 @@
 extends Control
-
 @export var offset:int = 40
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @export var text_edit: TextEdit
-@export var hide_start_windows_pos:Vector2i
-@export var hide_end_windows_pos:Vector2i
+
+@export var current_window_pos:Vector2i
+@export var start_window_pos:Vector2i
+@export var hiding_window_pos:Vector2i
+@export var peak_window_pos:Vector2i
+
+@export var animation_speed:float
+
+@onready var timer: Timer = $Timer
 
 var save_manager:SaveGame = SaveGame.new()
-var is_hiding:bool = false
+
 #-------------------------------------------------------------------------------
 
 
-func _unhandled_input(event: InputEvent) -> void:
+func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Save"):
 		_save_game()
 	
 	if event.is_action_pressed("Hide"):
-			#animation_player.play("Hide")
-			pass
+		if Setting.is_hiding == false:
+			Setting.is_hiding = true
+			_start_hiding()
+		else:
+			Setting.is_hiding = false
+			_cancel()
+
+	if event.is_action_pressed("Peak"):
+		_peaking()
 
 func _ready() -> void:
 	boot()
 	
 	_load_game()
-	
 
 func boot():
 	#var win_pos = DisplayServer.window_get_position()
@@ -37,6 +49,8 @@ func boot():
 	DisplayServer.window_set_position(right_pos)
 	
 	animation_player.play("Init")
+	
+	first_boot_for_window_pos_set()
 
 func _load_game() -> void:
 	text_edit.text = SaveGame.load_game().text_save
@@ -47,8 +61,60 @@ func _save_game() -> void:
 
 func _set_window_pos(pos: Vector2i):
 	DisplayServer.window_set_position(pos)
+#-------------------------------------------------------------------------------
+func _process(delta: float) -> void:
+	if Setting.is_running_hiding_animation:
+		DisplayServer.window_set_position(current_window_pos)
+#-------------------------------------------------------------------------------
+
+func first_boot_for_window_pos_set():
+	current_window_pos = DisplayServer.window_get_position()
+	start_window_pos = DisplayServer.window_get_position()
+	hiding_window_pos = DisplayServer.screen_get_size()
+	peak_window_pos = DisplayServer.screen_get_size() - Vector2i(32, 32)
+	
+
+
 
 #-------------------------------------------------------------------------------
+#Animation
+func _start_hiding():
+	var tween = create_tween()
+	Setting.is_running_hiding_animation = true
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(self, "current_window_pos", hiding_window_pos, animation_speed)
+	print(Setting.is_running_hiding_animation)
+	tween.chain().tween_callback(_stop_animation)
+
+func _cancel():
+	var tween = create_tween()
+	Setting.is_running_hiding_animation = true
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "current_window_pos", start_window_pos, animation_speed)
+	print(Setting.is_running_hiding_animation)
+	tween.chain().tween_callback(_stop_animation)
+
+func _peaking():
+	var tween = create_tween()
+	Setting.is_running_hiding_animation = true
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(self, "current_window_pos", peak_window_pos, animation_speed)
+	print(Setting.is_running_hiding_animation)
+	tween.chain().tween_callback(_stop_animation)
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+func _stop_animation():
+	Setting.is_running_hiding_animation = false
+	print(Setting.is_running_hiding_animation)
 
 func _on_text_edit_text_changed() -> void:
 	_save_game()
--- setting.gd
+++ setting.gd
@@ -1,9 +1,11 @@
 extends Node
 
+var is_hiding: bool = false
+var is_running_hiding_animation: bool = false
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ESC"):
 		get_tree().quit()
 
-	if event.is_action_pressed("Hide"):
-		pass
+	

13295ac92387447860939bfd9b957cf9218dbeec
Revo Wu <rev027w@gmail.com>
2025-02-18 11:33:48 +0800

    Add: add a new tool panel.

--- Main/peak_panel.gd
+++ Main/peak_panel.gd
@@ -1,8 +1,10 @@
 extends Panel
 class_name PeakPanel
 signal peak_panel_sig
+@onready var timer: Timer = $"../Timer"
 
 func _gui_input(event: InputEvent) -> void:
+	timer.stop()
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			peak_panel_sig.emit()
